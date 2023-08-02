743b3018fa56489571639ae8715617bf812ef983
batiukmaks <batiukmaks@gmail.com>
2023-08-02 12:00:11 +0300

    Add requirements.txt

--- my-parking/test_model.py
+++ my-parking/test_model.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO("runs/detect/train2/weights/best.pt")
+model = YOLO("../runs/detect/train6/weights/best.pt")
 
 model(source="./videos/car-out/IMG_0643.MOV", conf=0.3, iou=0.5, show=True)
